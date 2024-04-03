276467c773d9bd35d55ab4e42986d57a05b2c826
aidanb3765 <aidan.blaney@temple.edu>
2024-04-02 22:52:13 -0400

    Update App.js regarding countries
    Added entities for multiple countries on the map using the Philadelphia location as a jumping off point from previous code.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import { Cartesian3, Color } from "cesium";
 import { Viewer, Entity } from "resium";
 
 export default function App() {
+  const countries = [
+    { name: 'China', position: [116.3833, 39.9167] },
+    { name: 'India', position: [78.9629, 20.5937] },
+    { name: 'United States', position: [-95.7129, 37.0902] },
+    { name: 'Canada', position: [-106.3468, 56.1304] },
+    { name: 'Brazil', position: [-49.2654, -14.2350] },
+    { name: 'Russia', position: [105.3188, 61.5240] },
+    { name: 'Philadelphia', position:[-75.165222, 39.952583] }
+  ];
+
   return (
     <Viewer full>
+      {countries.map((country, index) => (
       <Entity
-        name="Philadelphia"
-        position={Cartesian3.fromDegrees(-75.165222, 39.952583)}
+        key={index}
+        name= {country.name}
+        position={Cartesian3.fromDegrees(country.position[0], country.position[1])}
         point={{ pixelSize: 10, color: Color.BLACK }}
-        description="hoge"
+        description={country.name}
       />
+      ))}
     </Viewer>
   );
 }

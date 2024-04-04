0ea9a59b3671aaf8c22d1cc2512d0f00a80a78c0
aidanb3765 <aidan.blaney@temple.edu>
2024-04-04 15:53:38 -0400

    Creation of Countries Class
    Created a new class for storing the information of the countries selected. The object for the country is called within the App class and used to pin locations on the interactive globe

---
+++ src/CountriesLog.js
@@ -0,0 +1,17 @@
+export default class CountriesLog {
+    constructor() {
+        this.countries = [
+            { name: 'China', position: [116.3833, 39.9167] },
+            { name: 'India', position: [78.9629, 20.5937] },
+            { name: 'United States', position: [-95.7129, 37.0902] },
+            { name: 'Canada', position: [-106.3468, 56.1304] },
+            { name: 'Brazil', position: [-49.2654, -14.2350] },
+            { name: 'Russia', position: [105.3188, 61.5240] },
+            { name: 'Philadelphia', position: [-75.165222, 39.952583] }
+        ];
+    }
+
+    getAllCountries() {
+        return this.countries;
+    }
+}
